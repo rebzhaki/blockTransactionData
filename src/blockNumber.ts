d7bcfe837094c24db7a8aeab12b98f910e653c1f
rebzhaki <rebzhaki@gmail.com>
2023-04-28 17:07:18 +0300

    access block numbers of specific transactions

--- src/blockNumber.ts
+++ src/blockNumber.ts
@@ -1,48 +1,97 @@
 import { ethers } from "ethers";
-import { getBlock, getTransactionHash, _wssProvider } from "./common";
+import {
+  getBlock,
+  getTransactionHash,
+  _wssProvider,
+  // getAllBlockNumbers,
+  getWalletTrans,
+  getTransactionSpecifiedData,
+} from "./common";
 import { Settings } from "./config";
 import { ExportToCsv } from "export-to-csv";
 
 export const BlockNumber = async () => {
   console.log("Hello World!");
+  let walletAddress = "0xC2b1fFAa03fde1Fc3d5C2Db1E4187e13F51f9D03";
+  let completeData = [];
 
   try {
-    const output = await getBlock(Settings.block_Number!);
-    const txHash = output?.transactions;
-    // console.log("output", txHash);
-    var data = [];
-    const options = {
-      fieldSeparator: ",",
-      quoteStrings: '"',
-      decimalSeparator: ".",
-      showLabels: true,
-      showTitle: true,
-      title: "My Awesome CSV",
-      useTextFile: false,
-      useBom: true,
-      useKeysAsHeaders: true,
-      // headers: ['Column 1', 'Column 2', etc...], <-- Won't work with useKeysAsHeaders present!
-      additionalHeaders: [
-        { columns: ["HeaderRow1Column1", "HeaderRow1Column2"] },
-        { columns: ["HeaderRow2Column1", "HeaderRow2Column2"] },
-      ],
-    };
+    let transData = await getTransactionSpecifiedData(walletAddress).then(
+      (result) => {
+        return result;
+      }
+    );
 
-    const csvExporter = new ExportToCsv(options);
+    const timestamp = "Apr-12-2023 04:15:12 PM +UTC";
+    const unixTimestamp = Math.floor(Date.parse(timestamp) / 1000);
 
-    for (let index = 0; index < txHash?.length!; index++) {
-      // console.log("TX DATA", txHash?.[0]!);
-      const receipt = await getTransactionHash(txHash?.[index]!);
+    for (let j = 0; j < transData.length; j++) {
+      const element = transData[j];
+      const timestamp: number | undefined = element.timestamp;
+      console.log("rr", timestamp);
 
-      data.push({
-        index: index,
-        TransactionHash: txHash?.[index]!,
-        GasLimit: receipt?.gasLimit.toNumber(),
-        GasPrice: ethers.utils.formatEther(receipt?.gasPrice!),
-      });
+      if (element.to == "0xC2b1fFAa03fde1Fc3d5C2Db1E4187e13F51f9D03") {
+        completeData.push({
+          hash: element.hash,
+          value: ethers.utils.formatEther(element.value) + " ETH",
+          address: element.from,
+        });
+      }
     }
-    var csvdata = csvExporter.generateCsv(data, true);
-    console.log("eeee", csvdata);
+
+    console.log("comp;lete", completeData);
+
+    //access block number for transactions in a wallet then access all transactions
+
+    // let blockNumber = await getWalletTrans(
+    //   "0xC2b1fFAa03fde1Fc3d5C2Db1E4187e13F51f9D03"
+    // ).then((result) => {
+    //   return result;
+    // });
+
+    // console.log("rr", blockNumber!);
+
+    // for (let j = 0; j < blockNumber.length; j++) {
+    //   const element = blockNumber[j];
+
+    //   const output = await getBlock(Settings.block_Number!);
+    //   const txHash = output?.transactions;
+    //   // console.log("output", txHash);
+    //   var data = [];
+    //   const options = {
+    //     fieldSeparator: ",",
+    //     quoteStrings: '"',
+    //     decimalSeparator: ".",
+    //     showLabels: true,
+    //     showTitle: true,
+    //     title: "My Awesome CSV",
+    //     useTextFile: false,
+    //     useBom: true,
+    //     useKeysAsHeaders: true,
+    //     // headers: ['Column 1', 'Column 2', etc...], <-- Won't work with useKeysAsHeaders present!
+    //     additionalHeaders: [
+    //       { columns: ["HeaderRow1Column1", "HeaderRow1Column2"] },
+    //       { columns: ["HeaderRow2Column1", "HeaderRow2Column2"] },
+    //     ],
+    //   };
+
+    //   const csvExporter = new ExportToCsv(options);
+
+    //   for (let index = 0; index < txHash?.length!; index++) {
+    //     // console.log("TX DATA", txHash?.[0]!);
+    //     const receipt = await getTransactionHash(txHash?.[index]!);
+
+    //     data.push({
+    //       index: index,
+    //       TransactionHash: txHash?.[index]!,
+    //       GasLimit: receipt?.gasLimit.toNumber(),
+    //       GasPrice: ethers.utils.formatEther(receipt?.gasPrice!),
+    //     });
+    //   }
+
+    //   var csvdata = csvExporter.generateCsv(data, true);
+    //   console.log("eeee", csvdata);
+    // }
   } catch (error) {
     console.log("errrrrr", error);
   }
--- src/common/common.ts
+++ src/common/common.ts
@@ -1,9 +1,23 @@
 import { ethers } from "ethers";
 import { Settings } from "../config";
 
+const startTime = Math.floor(Date.parse("2023-01-01") / 1000);
+const endTime = Math.floor(new Date("2023-04-27").getTime() / 1000);
+
 export const _wssProvider = new ethers.providers.WebSocketProvider(
   Settings.WSS_URL!
 );
+
+export const getWalletTrans = async (address: string) => {
+  const provider = new ethers.providers.EtherscanProvider(5);
+  let allAddresses = await provider.getHistory(address);
+  let block_Number = [];
+
+  for (let i = 0; i < allAddresses.length; i++) {
+    block_Number.push(allAddresses[i].blockNumber);
+  }
+  return block_Number;
+};
 
 export const getBlock = async (block_Number: any) => {
   try {
@@ -19,4 +33,29 @@ export const getTransactionHash = async (txHash: string) => {
   } catch (error) {
     console.log("error getting the txHash", error);
   }
+};
+
+export const getTransactionSpecifiedData = async (address: string) => {
+  // const startBlock = await _wssProvider.;
+  const provider = new ethers.providers.EtherscanProvider(5);
+  let allAddresses = await provider.getHistory(address);
+  let transdata = [];
+  let block_number = [];
+
+  // for (let i = 0; i < allAddresses.length; i++) {
+  //   block_number.push(allAddresses[i].blockNumber);
+  //   console.log("mm", block_number);
+  // }
+  // for (let n = 0; n < block_number; n++) {
+  //   const block = await _wssProvider._getBlock(n);
+  //   if (block.timestamp >= startTime) {
+  //     blockNumbers.push(n);
+  //   }
+  // }
+
+  for (let i = 0; i < allAddresses.length; i++) {
+    transdata.push(allAddresses[i]);
+    // console.log("alldataaa", allAddresses[i]);
+  }
+  return transdata;
 };
